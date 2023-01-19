eea3647dcc31550ea159536b7c13f1e28cb8267e
Jeff Kayombo <jeffkayombo16@gmail.com>
2023-01-20 00:42:55 +0200

    he

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import ReactGA from 'react-ga'
+
+ReactGA.initialize('UA-238822815-1')
 
 function App() {
 
