6bedd292fad6486dcd54b031f960da8076b6521e
Jeff Kayombo <jeffkayombo16@gmail.com>
2023-01-19 23:02:56 +0200

    Test

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,7 +15,7 @@ const Home = () => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Testing Analytics</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
